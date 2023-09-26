97a9d781d3a8ae4bd32aed958a5c83c37ea54f23
Haywar Zhu <haywar@Haywars-MacBook-Air.local>
2023-09-26 12:33:02 -0400

    add image and finish step2

--- krypto_jobs.py
+++ krypto_jobs.py
@@ -174,7 +174,7 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-eth_balance = get_balance(account.address)
+eth_balance = get_balance(w3,account.address)
 st.sidebar.write(f"Balance: {eth_balance} ETH")
 
 ##########################################
@@ -298,7 +298,11 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    # YOUR CODE HERE
+    if st.sidebar.button("Send Transaction"):
+        transaction_hash = send_transaction(account, candidate_address, wage)
+        st.sidebar.markdown("#### Validated Transaction Hash")
+        st.sidebar.write(transaction_hash)
+        st.balloons()
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
